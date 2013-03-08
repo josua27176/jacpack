861912f0cd4708062377c3d7ee2b201b7e12574f
Alexander Behrhof <traincrazyb@aol.com>
2013-03-07 20:40:33 -0500

    WorldGen.

--- src/minecraft/org/Jacpack/TechUp/block/ModBlocks.java
+++ src/minecraft/org/Jacpack/TechUp/block/ModBlocks.java
@@ -1,5 +1,20 @@
 package org.Jacpack.TechUp.block;
 
-public class ModBlocks {
+import net.minecraft.item.ItemStack;
 
+import org.Jacpack.TechUp.util.Config;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+
+public class ModBlocks {
+	
+	public static BlockCustomOre blockOres;
+    public static BlockStorage blockStorage;
+	
+	public static void init() {
+		blockOres = new BlockCustomOre(Config.getBlockID("blocks.world.ores.id"));
+        blockOres.setBlockName("TechUpores");
+        GameRegistry.registerBlock(blockOres, ItemCustomOre.class, "ores");
+	}
+	
 }
