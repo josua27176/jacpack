7e4f541f105e2e40809df7b3568cd86c8297dfa3
xFinityPro <xfinpro@gmail.com>
2013-03-09 17:46:23 -0800

    + Added Bronze, Steel, Copper, Silver, Hazmat recipe
    Added Armor Crafting recipes.

--- src/minecraft/org/Jacpack/TechUp/crafting/CraftingHandler.java
+++ src/minecraft/org/Jacpack/TechUp/crafting/CraftingHandler.java
@@ -17,6 +17,31 @@ public class CraftingHandler
         GameRegistry.addRecipe(new ItemStack(ModBlocks.blockOres, 1, 11), new Object[] {"GGG", "GGG", "GGG", 'G', ModItems.itemRefinedUranium});
         GameRegistry.addRecipe(new ItemStack(ModBlocks.blockOres, 1, 12), new Object[] {"GGG", "GGG", "GGG", 'G', ModItems.itemIngotSilver});
         GameRegistry.addRecipe(new ItemStack(ModBlocks.blockOres, 1, 13), new Object[] {"GGG", "GGG", "GGG", 'G', ModItems.itemAdemartium});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemBronzeHelment, 1), new Object[] {"GGG", "G G", 'G', ModItems.itemIngotBronze});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemBronzeChestplate, 1), new Object[] {"G G", "GGG", "GGG", 'G', ModItems.itemIngotBronze});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemBronzeLeggings, 1), new Object[] {"GGG", "G G", "G G", 'G', ModItems.itemIngotBronze});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemBronzeBoots, 1), new Object[] {"G G", "G G", 'G', ModItems.itemIngotBronze});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemSteelHelmet, 1), new Object[] {"GGG", "G G", 'G', ModItems.itemIngotSteel});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemSteelChestplate, 1), new Object[] {"G G", "GGG", "GGG", 'G', ModItems.itemIngotSteel});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemSteelLeggings, 1), new Object[] {"GGG", "G G", "G G", 'G', ModItems.itemIngotSteel});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemSteelBoots, 1), new Object[] {"G G", 'G', ModItems.itemIngotSteel});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemCopperHelment, 1), new Object[] {"GGG", "G G", 'G', ModItems.itemIngotCopper});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemCopperChestplate, 1), new Object[] {"G G", "GGG", "GGG", 'G', ModItems.itemIngotCopper});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemCopperLeggings, 1), new Object[] {"GGG", "G G", "G G", 'G', ModItems.itemIngotCopper});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemCopperBoots, 1), new Object[] {"G G", 'G', ModItems.itemIngotCopper});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemSilverHelment, 1), new Object[] {"GGG", "G G", 'G', ModItems.itemIngotSilver});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemSilverChestplate, 1), new Object[] {"G G", "GGG", "GGG", 'G', ModItems.itemIngotSilver});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemSilverLeggings, 1), new Object[] {"GGG", "G G", "G G", 'G', ModItems.itemIngotSilver});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemSilverBoots, 1), new Object[] {"G G", 'G', ModItems.itemIngotSilver});
+        
+        
+        //HAZMAT SUIT HAS NO ITEM TO BE CRAFTABLE
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemHazmatHelment, 1), new Object[] {"GGG", "G G", "G G", 'G', ModItems.itemIngotSilver});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemHazmatChesplate, 1), new Object[] {"GGG", "G G", "G G", 'G', ModItems.itemIngotSilver});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemHazmatLeggings, 1), new Object[] {"GGG", "G G", "G G", 'G', ModItems.itemIngotSilver});
+        GameRegistry.addRecipe(new ItemStack(ModItems.itemHazmatBoots, 1), new Object[] {"GGG", "G G", "G G", 'G', ModItems.itemIngotSilver});
+        
+        
         GameRegistry.addRecipe(CraftingHelper.copyStack(ModItems.itemIngotTitanium, 9), new Object[] {"G", 'G', new ItemStack(ModBlocks.blockOres, 1, 7)});
         GameRegistry.addRecipe(CraftingHelper.copyStack(ModItems.itemIngotAluminum, 9), new Object[] {"G", 'G', new ItemStack(ModBlocks.blockOres, 1, 8)});
         GameRegistry.addRecipe(CraftingHelper.copyStack(ModItems.itemIngotCopper, 9), new Object[] {"G", 'G', new ItemStack(ModBlocks.blockOres, 1, 9)});
