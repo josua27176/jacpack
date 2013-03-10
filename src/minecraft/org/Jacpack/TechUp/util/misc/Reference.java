4d3b78d641ec2d55cc76c542fb3b5844a5c1025f
Alexander Behrhof <traincrazyb@aol.com>
2013-03-10 12:57:37 -0400

    It says i changed a few things.

--- src/minecraft/org/Jacpack/TechUp/util/misc/Reference.java
+++ src/minecraft/org/Jacpack/TechUp/util/misc/Reference.java
@@ -16,7 +16,7 @@ public class Reference {
 
     /* General Mod related constants */
     public static final String MOD_ID = "TechUp";
-    public static final String MOD_NAME = "TechUp";
+    public static final String MOD_NAME = "Time-Traveler";
     public static final String VERSION = "0.0.0.1B";
     public static final String CHANNEL_NAME = MOD_ID;
     public static final String LOGGER_PREFIX = "[" + MOD_ID + "] ";
